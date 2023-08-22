7b6636fe53c0cd4d1225c86b14c309c70f14f280
Asit <34064195+officialasit@users.noreply.github.com>
2023-08-22 11:16:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import VitePWA from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Threejs-3D-Portfolio/',
-  plugins: [react()],
-})
+  plugins: [VitePWA()],
+  base: '/Threejs-3D-Portfolio/', // Adjust this to your repository name
+});
